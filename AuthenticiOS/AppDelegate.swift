ae6259cfae7e3333538e131313e5f347c1c3ddbc
Greg Whatley <devgregw@outlook.com>
2018-05-26 18:51:39 -0500

    Set up notifications

--- AuthenticiOS/AppDelegate.swift
+++ AuthenticiOS/AppDelegate.swift
@@ -20,12 +20,16 @@ extension AppDelegate : UNUserNotificationCenterDelegate {
                                 willPresent notification: UNNotification,
                                 withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
         //let userInfo = notification.request.content.userInfo
+        print("UN willPresent: \(notification.request.content.title)")
         completionHandler([.alert, .sound])
     }
     
     func userNotificationCenter(_ center: UNUserNotificationCenter,
                                 didReceive response: UNNotificationResponse,
                                 withCompletionHandler completionHandler: @escaping () -> Void) {
+        print("UN didReceive: \(response.notification.request.content.title)::\(response.actionIdentifier)")
+        self.application(UIApplication.shared, didReceiveRemoteNotification: response.notification.request.content.userInfo)
+        AppDelegate.invokeNotificationAction(withViewController: UIApplication.shared.keyWindow!.rootViewController!)
         completionHandler()
     }
 }
@@ -44,6 +48,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any]) {
+        print("didReceiveRemoteNotification")
         AppDelegate.notificationAction = AuthenticButtonAction(dict: NSDictionary(dictionary: userInfo.filter({ (arg) -> Bool in
             let (key, _) = arg
             return key as! String != "aps"
@@ -51,7 +56,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        print("didReceiveRemoteNotification completionHandler")
         self.application(application, didReceiveRemoteNotification: userInfo)
+        completionHandler(.newData)
     }
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
@@ -64,13 +71,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             print(error)
         }
         UIBarButtonItem.appearance().setTitleTextAttributes([NSAttributedStringKey.font : UIFont(name: "Proxima Nova", size: 18) ?? UIFont.systemFont(ofSize: 18)], for: .normal)
-        Messaging.messaging().subscribe(toTopic: "main")
         if #available(iOS 10.0, *) {
             UNUserNotificationCenter.current().delegate = self
             UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound], completionHandler: {_, _ in })
         } else {
             application.registerUserNotificationSettings(UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil))
         }
+        application.registerForRemoteNotifications()
         if let shortcut = launchOptions?[.shortcutItem] as? UIApplicationShortcutItem {
             self.launchedItem = shortcut
             _ = respondToShortcut()
@@ -80,24 +87,55 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Unable to register for notifications: \(error.localizedDescription)")
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        print("APNS token received: \(deviceToken)")
+        Messaging.messaging().subscribe(toTopic: "main")
+        //Messaging.messaging().apnsToken = deviceToken
+    }
+    
     func application(_ application: UIApplication, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
         self.launchedItem = shortcutItem
         completionHandler(respondToShortcut())
         self.launchedItem = nil
     }
     
+    public static func getTopmostViewController() -> UIViewController {
+        var vc = UIApplication.shared.keyWindow!.rootViewController!
+        while (vc.presentedViewController != nil) {
+            vc = vc.presentedViewController!
+        }
+        return vc
+    }
+    
+    public static func automaticPresent(viewController newVC: UIViewController) {
+        var vc = getTopmostViewController()
+        if let hp = vc as? ACHomePageViewController {
+            let index = ACHomePageViewController.controllers.index(of: hp.viewControllers!.first!)
+            if index == 0 {
+                let nvc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "hmroot")
+                vc.present(nvc, animated: true, completion: {
+                    nvc.show(newVC, sender: nil)
+                })
+            } else {
+                hp.viewControllers!.first!.show(newVC, sender: nil)
+            }
+            return
+        }
+        vc.show(newVC, sender: nil)
+    }
+    
     public func respondToShortcut() -> Bool {
         var handled = false
         if let item = self.launchedItem {
-            var vc = self.window!.rootViewController!
-            while (vc.presentedViewController != nil) {
-                vc = vc.presentedViewController!
-            }
             switch (item.type) {
             case "upcoming_events":
                 Database.database().reference().child("appearance").observeSingleEvent(of: .value, with: { appearanceSnapshot in
                     let appearance = AuthenticAppearance(dict: appearanceSnapshot.value as! NSDictionary)
-                    ACEventListController.present(withAppearance: appearance.events, viewController: vc)
+                    ACEventListController.present(withAppearance: appearance.events)
                 })
                 handled = true
                 break
@@ -105,15 +143,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 let tabId = item.userInfo!["id"] as! String
                 Database.database().reference().child("tabs/\(tabId)").observeSingleEvent(of: .value, with: {snapshot in
                     let val = snapshot.value as! NSDictionary
-                    if vc is ACHomeViewController {
+                    ACTabViewController.present(tab: AuthenticTab(dict: val))
+                    /*if vc is ACHomePageViewController {
                         let nvc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "hmroot")
                         vc.present(nvc, animated: true, completion: {
                             nvc.show(ACTabViewController(tab: AuthenticTab(dict: val)), sender: nil)
                         })
                     } else {
                         vc.show(ACTabViewController(tab: AuthenticTab(dict: val)), sender: nil)
-                    }
-                }) { error in vc.present(UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert), animated: true) }
+                    }*/
+                }) { error in AppDelegate.getTopmostViewController().present(UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert), animated: true) }
                 handled = true
                 break
             default:
@@ -122,31 +161,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
         return handled
     }
-    
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-    
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
         _ = respondToShortcut()
         self.launchedItem = nil
     }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
 }
-
