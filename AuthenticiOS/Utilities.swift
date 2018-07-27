5139ecc4ae894d91b416c35b89a83a7a462f1791
Greg Whatley <devgregw@outlook.com>
2018-07-27 13:51:00 -0500

    Project update and version increment

--- AuthenticiOS/Utilities.swift
+++ AuthenticiOS/Utilities.swift
@@ -69,7 +69,7 @@ extension NSDictionary {
 
 class VersionInfo {
     static let Version = "1.0"
-    static let Update = 7
+    static let Update = 8
     static let FullVersion = "\(Version) build \(Update)"
 }
 
