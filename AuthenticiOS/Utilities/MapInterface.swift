539f4335eeaa50c84829e018d72b037e15e127e5
Greg Whatley <devgregw@outlook.com>
2019-01-01 16:26:43 -0600

    Added detailed messages
    ...for GetDirectionsAction and ShowMapAction

--- AuthenticiOS/Utilities/MapInterface.swift
+++ AuthenticiOS/Utilities/MapInterface.swift
@@ -18,13 +18,13 @@ class MapInterface {
         return UIApplication.shared.canOpenURL(URL(string: "http://maps.apple.com")!)
     }
     
-    private static func queryMap(apple: URL, google: URL) {
+    private static func queryMap(search: Bool, place: String, apple: URL, google: URL) {
         let gmaps = isGoogleMapsAvailable()
         let amaps = isAppleMapsAvailable()
         let openApple = {UIApplication.shared.open(apple, options: [:], completionHandler: nil)}
         let openGoogle = {UIApplication.shared.open(google, options: [:], completionHandler: nil)}
         if gmaps && amaps {
-            let alert = UIAlertController(title: "Select App", message: nil, preferredStyle: .actionSheet)
+            let alert = UIAlertController(title: search ? "Search Map" : "Get Directions", message: search ? "Select an app to search for \(place)." : "Select an app to get directions to \(place).", preferredStyle: .actionSheet)
             let topmostView = AppDelegate.getTopmostViewController().view!
             alert.popoverPresentationController?.sourceView = topmostView
             alert.popoverPresentationController?.sourceRect = CGRect(x: topmostView.bounds.midX, y: topmostView.bounds.midY, width: 0, height: 0)
@@ -43,10 +43,10 @@ class MapInterface {
     }
     
     static func getDirections(toAddress address: String) {
-        queryMap(apple: URL(string: "http://maps.apple.com/?daddr=\(address.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!)")!, google: URL(string: "https://www.google.com/maps/dir/?api=1&destination=\(address.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!)")!)
+        queryMap(search: false, place: address, apple: URL(string: "http://maps.apple.com/?daddr=\(address.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!)")!, google: URL(string: "https://www.google.com/maps/dir/?api=1&destination=\(address.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!)")!)
     }
     
     static func search(forPlace location: String) {
-        queryMap(apple: URL(string: "http://maps.apple.com/?q=\(location.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!)")!, google: URL(string: "https://www.google.com/maps/search/?api=1&query=\(location.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!)")!)
+        queryMap(search: true, place: location, apple: URL(string: "http://maps.apple.com/?q=\(location.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!)")!, google: URL(string: "https://www.google.com/maps/search/?api=1&query=\(location.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!)")!)
     }
 }
