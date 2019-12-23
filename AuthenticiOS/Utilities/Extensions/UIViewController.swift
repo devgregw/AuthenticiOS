ef577f1b4093a83655bcc36873b2228e1314cf6c
Greg Whatley <me@gregwhatley.dev>
2019-12-22 21:05:43 -0600

    Update UIViewController.swift

--- AuthenticiOS/Utilities/Extensions/UIViewController.swift
+++ AuthenticiOS/Utilities/Extensions/UIViewController.swift
@@ -8,6 +8,12 @@
 
 import UIKit
 
+enum ACRefreshControlStyle {
+    case automatic
+    case light
+    case dark
+}
+
 extension UIViewController {
     func applyDefaultAppearance() {
         // Remove back button label and make the arrow white
@@ -20,9 +26,44 @@ extension UIViewController {
         navigationController?.navigationBar.isTranslucent = false
     }
     
+    func setupRefreshControl(for scroll: UIScrollView, selector: Selector, style: ACRefreshControlStyle = .automatic) {
+        var color: UIColor
+        if style == .automatic {
+            if #available(iOS 13.0, *) {
+                color = traitCollection.userInterfaceStyle == .dark ? .white : .black
+            } else {
+                color = .white
+            }
+        } else {
+            color = style == .light ? .white : .black
+        }
+        if scroll.refreshControl == nil {
+            scroll.refreshControl = UIRefreshControl()
+        }
+        scroll.refreshControl?.tintColor = color
+        scroll.refreshControl?.addTarget(self, action: selector, for: .valueChanged)
+        scroll.refreshControl?.attributedTitle = NSAttributedString(string: "PULL TO REFRESH", attributes: [
+            //.kern: 2.5,
+            .font: UIFont(name: "Alpenglow-ExpandedRegular", size: 12)!,
+            .foregroundColor: color
+        ])
+    }
+    
     // This method makes decisions based on the top (or most recently presented) view controller
     // to figure out how to present self correctly
     func presentSelf(sender: Any?) {
         AppDelegate.topViewController.show(self, sender: sender)
+    }
+}
+
+extension UICollectionViewController {
+    func setupRefreshControl(selector: Selector, style: ACRefreshControlStyle = .automatic) {
+        setupRefreshControl(for: collectionView, selector: selector, style: style)
+    }
+}
+
+extension UITableViewController {
+    func setupRefreshControl(selector: Selector, style: ACRefreshControlStyle = .automatic) {
+        setupRefreshControl(for: tableView, selector: selector, style: style)
     }
 }
